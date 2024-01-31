3c9db843abee00560534ba021bccc8ba3959f762
kevin Gates <kevinlukanga@gmail.com>
2024-01-31 14:42:36 +0100

    update : script pour RHEL

--- install-jdk.sh
+++ install-jdk.sh
@@ -144,7 +144,7 @@ elif [[ "$distro" == *"$rhel"* ]]; then
 
 echo "[*]: uninstall openJDK"
 
-remouv= "dnf remove java-* -y"
+remouv= "dnf remove --purge java-* -y"
 $remouv
 
 echo "[**]: install openJDK"
@@ -197,7 +197,7 @@ elif [[ "$distro" == *"$rhel"* ]]; then
 
 echo "[*]: uninstall openJDK"
 
-remouv= "dnf remove java-* -y"
+remouv= "dnf remove --purge java-* -y"
 $remouv
 
 echo "[**]: install openJDK"
